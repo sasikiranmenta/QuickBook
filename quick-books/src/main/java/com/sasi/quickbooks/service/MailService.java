63ad565f8835ac17bfacd0d96466b1eca76c802b
sasikiranmenta <sasikiranmenta@users.noreply.github.com>
2023-04-02 16:02:17 +0530

    Mongo invoice changes

--- quick-books/src/main/java/com/sasi/quickbooks/service/MailService.java
+++ quick-books/src/main/java/com/sasi/quickbooks/service/MailService.java
@@ -9,8 +9,6 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
-import javax.mail.Message;
-import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.io.File;
 
@@ -24,6 +22,7 @@ public class MailService {
         this.mailSender = mailSender;
     }
 
+    @SuppressWarnings("unused")
     public void sendSimpleMailMessage(String from, String to, String subject, String text) {
         try {
             SimpleMailMessage mailMessage = new SimpleMailMessage();
@@ -38,22 +37,17 @@ public class MailService {
         }
     }
 
-    public boolean sendMailWithAttachment(String to, String subject, String body, File attachment1, File attachment2)
-    {
-        MimeMessagePreparator preparator = new MimeMessagePreparator()
-        {
-            public void prepare(MimeMessage mimeMessage) throws Exception
-            {
-                MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-                helper.setTo(to);
-                helper.setFrom("xyz@gmail.com");
-                helper.setText(body);
-                mimeMessage.setSubject(subject);
-                FileSystemResource file1 = new FileSystemResource(attachment1);
-                FileSystemResource file2 = new FileSystemResource(attachment2);
-                helper.addAttachment("noGst.pdf", file1);
-                helper.addAttachment("onlyGst.pdf", file2);
-            }
+    public boolean sendMailWithAttachment(String to, String subject, String body, File attachment1, File attachment2) {
+        MimeMessagePreparator preparator = (MimeMessage mimeMessage) -> {
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+            helper.setTo(to);
+            helper.setFrom("xyz@gmail.com");
+            helper.setText(body);
+            mimeMessage.setSubject(subject);
+            FileSystemResource file1 = new FileSystemResource(attachment1);
+            FileSystemResource file2 = new FileSystemResource(attachment2);
+            helper.addAttachment("noGst.pdf", file1);
+            helper.addAttachment("onlyGst.pdf", file2);
         };
         try {
             mailSender.send(preparator);
